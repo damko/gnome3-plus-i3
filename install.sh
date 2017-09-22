d119033278cbaba24b9a43e2415dc4737fe44e2f
Damiano Venturin <dam@venturin.net>
2017-09-22 13:56:52 +0200

    improved install and uninstall

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then 
+    echo "Please run as root"
+    exit 1
+fi
+
 cp -f usr/bin/gnome3-launcher /usr/bin/
 cp -f usr/bin/i3-launcher /usr/bin/
 cp -f usr/share/applications/i3-launcher.desktop /usr/share/applications/
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-rm usr/bin/gnome3-launcher /usr/bin/
-rm usr/bin/i3-launcher /usr/bin/
-rm usr/share/applications/i3-launcher.desktop /usr/share/applications/
-rm usr/share/gnome-session/sessions/gnome3-plus-i3.session /usr/share/gnome-session/sessions/
-rm usr/share/xsessions/gnome3-plus-i3.desktop /usr/share/xsessions/
+if [[ $EUID -ne 0 ]]; then 
+    echo "Please run as root"
+    exit 1
+fi
+
+rm usr/bin/gnome3-launcher
+rm usr/bin/i3-launcher
+rm usr/share/applications/i3-launcher.desktop
+rm usr/share/gnome-session/sessions/gnome3-plus-i3.session
+rm usr/share/xsessions/gnome3-plus-i3.desktop
